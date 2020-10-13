c30daaa3e1f0338bed9131bb518a62e0735125c5
robinGiri <girirobin2018@gmail.com>
2020-10-13 21:49:45 +0545

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:tarvelApp/screens/Home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Visit Nepal 2020',
+      theme: ThemeData(
+        primaryColor: Color(0xFF3EBACE),
+        accentColor: Color(0xFFD8ECF1),
+        scaffoldBackgroundColor: Color(0xFFF3F5F7),
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
---
+++ lib/widget/DestinationCarousal.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:tarvelApp/models/destination_model.dart';
+
+class DestinationCarousal extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Top Destinations',
+                style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 1.5),
+              ),
+              GestureDetector(
+                onTap: () {},
+                child: Text(
+                  'See All',
+                  style: TextStyle(
+                      color: Theme.of(context).primaryColor,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                      letterSpacing: 1.0),
+                ),
+              )
+            ],
+          ),
+        ),
+        Container(
+          height: 300,
+          color: Colors.blue,
+          child: ListView.builder(
+              itemCount: destinations.length,
+              itemBuilder: (BuildContext context, int index) {}),
+        ),
+      ],
+    );
+  }
+}
